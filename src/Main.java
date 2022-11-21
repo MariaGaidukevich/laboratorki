c63419b3a36d3521d06c520a8d361b1cc78d222d
MariaGaidukevich <its_kovich@mail.ru>
2022-11-21 21:10:27 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -38,5 +38,16 @@ public class Main {
         if (b % 2 == 0 && b > 100) {
             System.out.println("Выход за пределы диапазона");
         }
+        //Lab2
+        //Task 1
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(485842415859L, 55554334435L));
+        System.out.println(calculator.sub(4.5, 5.5));
+        System.out.println(calculator.mul(1000, 1000));
+        System.out.println(calculator.div(5, 2));
+
+        //Task 2
+        Chocolate milk = new Chocolate("milk", 30, false);
+        milk.info();
     }
 }
